00eb5dda3a15b5b76cd691bb39f973c3a7118a6f
Rajat <rajatkumar100095@gmail.com>
2024-01-05 22:51:02 +0530

    Home page created for both student and admin

---
+++ Admin/client/src/App.js
@@ -0,0 +1,37 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="adminHome">
+      <header className="head">
+        <div className="college-name">Admin</div>
+        <nav>
+          <a href="/">Home</a>
+          <a href="/">Contact</a>
+          <a href="/">About</a>
+        </nav>
+      </header>
+
+      <div className="hero-section">
+        <div>
+          <h1>Course Registration</h1>
+        </div>
+        <div className="dropdown">
+          <button className="dropbtn">Select Semester</button>
+          <div className="dropdown-content">
+            <a href="/">1st Semester</a>
+            <a href="/">2nd Semester</a>
+            <a href="/">3rd Semester</a>
+            <a href="/">4th Semester</a>
+            <a href="/">5th Semester</a>
+            <a href="/">6th Semester</a>
+            <a href="/">7th Semester</a>
+            <a href="/">8th Semester</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
