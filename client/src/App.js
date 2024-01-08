6857890f103ba0b57a418e75ba080da208905d4b
Rajat <rajatkumar100095@gmail.com>
2024-01-09 01:24:46 +0530

    Frontend for login page completed.

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,26 @@ import { React, useState } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Hero from './components/Hero';
+import Login from './components/Login';
+import AdminHero from './components/admin/AdminHero';
 import Status from './components/admin/Status';
+import StudentHero from './components/student/StudentHero';
 
 function App() {
   const [sem, setSem] = useState(0);
+  const [adminSession, setAdminSession] = useState(0);
+  const toggleAdminSession=()=>{
+    (adminSession === 0) ? setAdminSession(1) : setAdminSession(0);
+  };
 
   return (
     <div>
       <Router>
         <Navbar />
         <Routes>
-          <Route exact path='/' element={<Hero setSem={setSem} />}></Route>
+          <Route exact path='/' element={<Login adminSession={adminSession} toggleAdminSession={toggleAdminSession} />}></Route>
+          <Route exact path='/adminhero' element={<AdminHero setSem={setSem} />}></Route>
+          <Route exact path='/studenthero' element={<StudentHero setSem={setSem} />}></Route>
           <Route exact path='/status' element={<Status sem={sem} />}></Route>
         </Routes>
       </Router>
