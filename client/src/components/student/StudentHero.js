42a97b32e6070df9784f1fe65168cea47f7ffb65
Rajat <rajatkumar100095@gmail.com>
2024-02-26 13:32:25 +0530

    Receipt upload mechanism completed from student side.

--- client/src/components/student/StudentHero.js
+++ client/src/components/student/StudentHero.js
@@ -17,6 +17,10 @@ function StudentHero(props) {
       document.getElementById('allotted').style.display = 'block';
       document.getElementById('payStatus').style.display = 'block';
     }
+    if (data.status === 400) {
+      document.getElementById('courseRegBtn').style.display = 'none';
+      document.getElementById('underVerification').style.display = 'block';
+    }
   }
 
   useEffect(() => {
@@ -37,6 +41,7 @@ function StudentHero(props) {
       <div className="options" id="courseRegBtn"><button className="option" onClick={() => { navigate('/regpage') }}>Course Registration</button></div>
       <div className="options" id="payStatus" style={{ display: "none" }}><button className="option" onClick={() => { navigate('/receipt') }}>View Payment Receipt</button></div>
       <div className="options" id="allotted" style={{ display: "none" }}><button className="option">View Allotted Courses</button></div>
+      <div className="options" id="underVerification" style={{ display: "none" }}>Payment status under verification...</div>
     </div>
   );
 }
