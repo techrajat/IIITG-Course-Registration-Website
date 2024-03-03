fafe6a5c7c91af00ec9beff444bf77ddd1533901
Rajat <rajatkumar100095@gmail.com>
2024-03-03 18:17:30 +0530

    Feature for giving preferences for multiple electives added.

--- client/src/components/admin/Status.js
+++ client/src/components/admin/Status.js
@@ -66,6 +66,7 @@ function Status(props) {
                             <th scope="col">S. no.</th>
                             <th scope="col">Roll no.</th>
                             <th scope="col">Name</th>
+                            <th scope="col">CPI</th>
                             <th scope="col">Selected electives</th>
                             <th scope="col">Allotted electives</th>
                         </tr>
@@ -76,6 +77,7 @@ function Status(props) {
                                 <td>{index + 1}</td>
                                 <td>{element.roll_number}</td>
                                 <td>{element.name}</td>
+                                <td>{element.cpi}</td>
                                 <td>{!element.selected_elective ? "NA" : element.selected_elective.map((e)=>{return <>{`${e.code}: ${e.name}`}<br /></>;})}</td>
                                 <td>{!element.allotted_elective ? "NA" : element.allotted_elective}</td>
                             </tr>
